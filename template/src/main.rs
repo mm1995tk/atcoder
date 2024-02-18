112547a5a8f7393c9a77a9e81ec11d06eb761b29
mm1995tk <jimi.woodstock1995@gmail.com>
2024-02-18 19:30:21 +0900

    数学的問題を解くときの関数を追加

--- template/src/main.rs
+++ template/src/main.rs
@@ -12,7 +12,6 @@ use std::{
 fn main() {
     input! {
         n:usize,
-        edges: [(usize, usize);n]
     }
 }
 
@@ -34,6 +33,28 @@ fn run_length_encoding(str: &str) -> Vec<(char, usize)> {
         }
     }
     vec
+}
+
+/// 繰り返し二乗法によるa^nをpで割ったあまりを求める
+fn mod_pow(a: usize, n: usize, p: usize) -> usize {
+    if n == 0 {
+        return 1;
+    }
+    if n == 1 {
+        return a % p;
+    }
+
+    if n % 2 == 1 {
+        (a % p * mod_pow(a, n - 1, p)) % p
+    } else {
+        let t = mod_pow(a, n / 2, p) % p;
+        (t * t) % p
+    }
+}
+
+/// 体がmod(p)の時のa/bの結果を求める
+fn div_mod(a: usize, b: usize, p: usize) -> usize {
+    a * mod_pow(b, p - 2, p)
 }
 
 /// ユークリッド距離の2乗
